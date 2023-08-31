f1c6f2e8d9fa18777dbad5925722a7415ec0fda8
js012108 <js012108@gmail.com>
2023-08-30 20:20:44 -0500

    Clase 1

---
+++ e1.py
@@ -0,0 +1,5 @@
+x=int(input('Ingrese un numero: '))
+if x>=0:
+	print('El numero es positivo')
+else:
+	print('El numero es negativo')
---
+++ prueba.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hola, hoy es miercoles\n");
+  return 0;
+}
